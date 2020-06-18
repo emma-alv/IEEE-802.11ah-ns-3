ab79c0a1f27aa288cda9c195d5b22beba3df2733
Emmanuel Rodriguez <ing.erodrigueza@gmail.com>
2020-06-18 21:37:10 +0300

    fix log error

--- simulation_script.sh
+++ simulation_script.sh
@@ -63,8 +63,7 @@ fi
 Name="$distance-m-$NumSta-sta-$time-time"
 
 ./waf --run "test --seed=1 --simulationTime=$time --payloadSize=$payloadSize --Nsta=$NumSta --pagePeriod=$pageSliceCount --pageSliceLength=$pageSliceLen --pageSliceCount=$pageSliceCount \
---NRawSlotNum=$NumSlot --NGroup=$NRawGroups --rho=$distance --bandWidth=$bandWidth --DataMode=$DataMode --TrafficType=$TrafficType --RAWConfigFile=$RAWConfigPath --Name=$Name" \
-./results/$DataMode/$TrafficType/RAW_G_$NRawGroups/RAW_S_$NumSlot/simlog_$NOW.txt 2>&1
+--NRawSlotNum=$NumSlot --NGroup=$NRawGroups --rho=$distance --bandWidth=$bandWidth --DataMode=$DataMode --TrafficType=$TrafficType --RAWConfigFile=$RAWConfigPath --Name=$Name" > ./results/$DataMode/$TrafficType/RAW_G_$NRawGroups/RAW_S_$NumSlot/simlog_$NOW.txt 2>&1
 
 echo "./results/$DataMode/$TrafficType/RAW_G_$NRawGroups/RAW_S_$NumSlot/simlog_$NOW.txt"
 echo "Simulation Done"
