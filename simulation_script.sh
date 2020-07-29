6ded584fec21d5e77144f5b24420f9856ff4bfae
Emmanuel Rodriguez <ing.erodrigueza@gmail.com>
2020-07-29 20:09:36 +0300

    Fixed relative path to absolute path

--- simulation_script.sh
+++ simulation_script.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-max_sta=256
+user=$( whoami )
+
+path_folder="/home/${user}/IEEE-802.11ah-ns-3"
+
 NumSta=$1
 NRawGroups=4
 NumSlot=4
@@ -15,6 +18,8 @@ TrafficType="udpecho"
 #tcpipcamera, udpecho
 NOW=$(date +"%F_%H_%M_%S")
 
+max_sta=$(( pageSliceLen * 64 ))
+
 pageSliceCount=$(( NumSta/max_sta ))
 mod=$(( NumSta%max_sta ))
 
@@ -25,42 +30,42 @@ fi
 
 echo $pageSliceCount
 
-RAWConfigPath="./OptimalRawGroup/RawConfig-$NumSta-$NRawGroups-$NumSlot-$beaconinterval-$pageSliceCount-$pageSliceLen.txt"
+RAWConfigPath="$path_folder/OptimalRawGroup/RawConfig-$NumSta-$NRawGroups-$NumSlot-$beaconinterval-$pageSliceCount-$pageSliceLen.txt"
 
-./waf --run "RAW-generate --NRawSta=$NumSta --NGroup=$NRawGroups --NumSlot=$NumSlot --RAWConfigPath=$RAWConfigPath --beaconinterval=$beaconinterval \
+$path_folder/waf --run "RAW-generate --NRawSta=$NumSta --NGroup=$NRawGroups --NumSlot=$NumSlot --RAWConfigPath=$RAWConfigPath --beaconinterval=$beaconinterval \
 --pageSliceCount=$pageSliceCount --pageSliceLen=$pageSliceLen"
 
 echo $RAWConfigPath
 
 
-if [ ! -d ./results/ ]
+if [ ! -d $path_folder/results/ ]
 then
-    mkdir ./results/
+    mkdir $path_folder/results/
 fi
 
-if [ ! -d ./results/logs ]
+if [ ! -d $path_folder/results/logs ]
 then
-    mkdir ./results/logs
+    mkdir $path_folder/results/logs
 fi
 
-if [ ! -d ./results/$DataMode ]
+if [ ! -d $path_folder/results/$DataMode ]
 then
-    mkdir ./results/$DataMode
+    mkdir $path_folder/results/$DataMode
 fi
 
-if [ ! -d ./results/$DataMode/$TrafficType ]
+if [ ! -d $path_folder/results/$DataMode/$TrafficType ]
 then
-    mkdir ./results/$DataMode/$TrafficType
+    mkdir $path_folder/results/$DataMode/$TrafficType
 fi
 
-if [ ! -d ./results/$DataMode/$TrafficType/RAW_G_$NRawGroups ]
+if [ ! -d $path_folder/results/$DataMode/$TrafficType/RAW_G_$NRawGroups ]
 then
-    mkdir ./results/$DataMode/$TrafficType/RAW_G_$NRawGroups
+    mkdir $path_folder/results/$DataMode/$TrafficType/RAW_G_$NRawGroups
 fi
 
-if [ ! -d ./results/$DataMode/$TrafficType/RAW_G_$NRawGroups/RAW_S_$NumSlot ]
+if [ ! -d $path_folder/results/$DataMode/$TrafficType/RAW_G_$NRawGroups/RAW_S_$NumSlot ]
 then
-    mkdir ./results/$DataMode/$TrafficType/RAW_G_$NRawGroups/RAW_S_$NumSlot
+    mkdir $path_folder/results/$DataMode/$TrafficType/RAW_G_$NRawGroups/RAW_S_$NumSlot
 fi
 
 date
@@ -68,10 +73,10 @@ date
 
 Name="$distance-m-$NumSta-sta-$time-time"
 
-./waf --run "test --seed=1 --simulationTime=$time --payloadSize=$payloadSize --Nsta=$NumSta --pagePeriod=$pageSliceCount --pageSliceLength=$pageSliceLen --pageSliceCount=$pageSliceCount \
---NRawSlotNum=$NumSlot --NGroup=$NRawGroups --rho=$distance --bandWidth=$bandWidth --DataMode=$DataMode --TrafficType=$TrafficType --RAWConfigFile=$RAWConfigPath --Name=$Name" > ./results/logs/simlog_$NOW.txt 2>&1
+$path_folder/waf --run "test --seed=1 --simulationTime=$time --payloadSize=$payloadSize --Nsta=$NumSta --pagePeriod=$pageSliceCount --pageSliceLength=$pageSliceLen --pageSliceCount=$pageSliceCount \
+--NRawSlotNum=$NumSlot --NGroup=$NRawGroups --rho=$distance --bandWidth=$bandWidth --DataMode=$DataMode --TrafficType=$TrafficType --RAWConfigFile=$RAWConfigPath --Name=$Name" > $path_folder/results/logs/simlog_$NOW.txt 2>&1
 
-echo "RAW PATH = ./OptimalRawGroup/RawConfig-$NumSta-$NRawGroups-$NumSlot-$beaconinterval-$pageSliceCount-$pageSliceLen.txt" >> ./results/logs/simlog_$NOW.txt
+echo "RAW PATH = ./OptimalRawGroup/RawConfig-$NumSta-$NRawGroups-$NumSlot-$beaconinterval-$pageSliceCount-$pageSliceLen.txt" >> $path_folder/results/logs/simlog_$NOW.txt
 echo "./results/logs/simlog_$NOW.txt"
 echo "Simulation Done"
 
@@ -81,7 +86,7 @@ lastSimulation=$(ls -trh | tail -n 1)
 
 if [[ $lastSimulation == *"$TrafficType"* ]]
 then
-    mv $lastSimulation ./results/$DataMode/$TrafficType/RAW_G_$NRawGroups/RAW_S_$NumSlot/$Name.nss
+    mv $lastSimulation $path_folder/results/$DataMode/$TrafficType/RAW_G_$NRawGroups/RAW_S_$NumSlot/$Name.nss
     echo $Name
 else
     echo "Error copying results"
